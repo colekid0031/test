57093d8601bff8620858218ceecfcdea37d2f236
Cole Kid <colekid0031@gmail.com>
2026-09-14 11:26:44 +0000

    basic DOM munipulation direct object model odin project

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-let prices = [2, 4, 6, 8, 10, 12, 14, 16,];
+const redP = document.createElement('p');
+redP.classList.add('red');
+redP.textContent = 'Hey i m red';
+redP.style.color = 'red';
+
+const blueH3 = document.createElement('h3');
+blueH3.classList.add('blue');
+blueH3.textContent = 'I’m a blue h3!'
+blueH3.style.color = 'blue';
+
+const Darkdiv = document.createElement('div');
+Darkdiv.classList.add('dark');
+Darkdiv.style.border = '4px solid black';
+Darkdiv.style.backgroundColor = 'pink';
+// Darkdiv.style.width = '200px';
+// Darkdiv.style.height = '200px';
 
 
-prices.forEach((price, i, arr)=>{
-    arr[i] = price * 0.5
-})
+const innerH1 = document.createElement('h1');
+innerH1.textContent = "i'm in a div";
+
+const innerP = document.createElement('p');
+innerP.textContent = "ME TOO";
+
+Darkdiv.appendChild(innerH1);
+Darkdiv.appendChild(innerP);
+
+document.body.appendChild(redP);
+
+document.body.appendChild(blueH3);
+
+document.body.appendChild(Darkdiv);
+
+console.log()
+var j = 2 + 2;
+// console.log(j);
+
+ 
+
+// your JavaScript file
+// const container = document.querySelector("#container");
+
+// const content = document.createElement("div");
+// content.classList.add("content");
+// content.textContent = "This is the glorious text-content!";
+
+// container.appendChild(content);
+
+
+
+
+
+
